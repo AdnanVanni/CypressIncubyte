a8259e9616dbb35c92918976f4f02902609a639c
AdnanVanni <adnanvanni@gmail.com>
2025-01-07 14:59:06 +0530

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,22 +1,31 @@
 const { defineConfig } = require("cypress");
-const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
-const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
-const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild");
+const createBundler = require('@bahmutov/cypress-esbuild-preprocessor');
+const createEsbuildPlugin = require('@bahmutov/cypress-esbuild-plugin');
+const preprocessor = require('@badeball/cypress-cucumber-preprocessor');
+const cypressSplit = require("cypress-split");
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      on("file:preprocessor",
-      createBundler({
-        plugins: [createEsbuildPlugin.default(config)],
-      }));
+      // First, use the esbuild plugin for pre-processing
+      on("file:preprocessor", 
+        createBundler({
+          plugins: [createEsbuildPlugin.default(config)],
+        })
+      );
+      
+      // Add the Cucumber preprocessor plugin
       preprocessor.addCucumberPreprocessorPlugin(on, config);
+      
+      // Then, use cypress-split plugin
+      cypressSplit(on, config);
+      
+      // Return the updated config
       return config;
     },
-	
-  specPattern: [ "**/signup.feature", "**/login.feature" ]
- 
+    
+    specPattern: ["**/signup.feature", "**/login.feature"]
+    //test
+    
   },
- 
-
-})
+});
